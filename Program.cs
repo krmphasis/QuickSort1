bb7b831d12d0e7264fbe28bf55b9c41ae759b3d1
kunalrelan <relan.krunal@gmail.com>
2023-06-15 09:23:05 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using QuickSort;
+
+//Console.WriteLine("Hello, World!");
+//Console.ReadLine();
+
+
+
+int[] arr = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+QuickSortLogic.QuickSort(arr, 0, arr.Length - 1);
+Console.WriteLine($"Sorted Array");
+foreach (int num in arr)
+{
+    Console.WriteLine(num);
+}
+
+Console.ReadLine();
---
+++ QuickSortLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace QuickSort
+{
+	public class QuickSortLogic
+	{
+        public static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(arr, left, right);
+                QuickSort(arr, left, pivotIndex - 1);
+                QuickSort(arr, pivotIndex + 1, right);
+            }
+        }
+
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int i = left - 1;
+            for (int j = left; j < right; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+            Swap(arr, i + 1, right);
+            return i + 1;
+        }
+
+        private static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
+
